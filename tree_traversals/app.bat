a5c9817b5537ea96995a8b399258e26fecd4039a
Tarun <tarunsrisaiv03@gmail.com>
2023-08-16 19:08:01 +0530

    Add lib to compilation

--- tree_traversals/app.bat
+++ tree_traversals/app.bat
@@ -8,7 +8,7 @@ IF "%~1"=="" (
 IF /I "%~1"=="run" (
     java -cp bin Main.java
 ) ELSE IF /I "%~1"=="build" (
-    javac src/*.java -d bin
+    javac lib/*.java src/*.java -d bin
 ) ELSE IF /I "%~1"=="clean" (
     del /q /s bin\*.class
 ) ELSE (
